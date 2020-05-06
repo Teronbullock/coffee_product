c909eeb216457731a47f9d4976e00f45ea6e31da
Teron Bullock <teron.bullock@gmail.com>
2020-05-06 14:52:58 -0400

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+/* ================================= 
+  app.js
+==================================== */
+const mobileNav = document.getElementById('nav__mobile');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenu.addEventListener('click', () => {
+    if (mobileNav.style.display === 'block' || screen.width >= 768) {
+        mobileNav.style.display = 'none';
+    } else if (window.screen.width < 768) {
+        mobileNav.style.display = 'block';
+    }
+});
+
+/* Hide Mobile-Nav above 768px */
+if (window.screen.width >= 768) {
+    mobileNav.style.display = 'none';
+}
